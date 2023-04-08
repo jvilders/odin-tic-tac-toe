ead07c6bb7d41a3c324eccf8422c8245ee989995
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-04-08 15:33:59 +0200

    Add AI player
    Implementing minimax gave me a huge headache. Initially I wanted to
    pre-calculate all optimal moves for the AI( "O") at every possible
    state of the board after a move by the player ("X"). Decided against
    this partly out of laziness, partly not wanting to run such a big
    amount of blocking computation client-side.

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ enum cellContent {
 
 interface cell {
   getValue(): cellContent;
-  changeValue(newValue: cellContent.Crosses | cellContent.Noughts): void;
+  changeValue(newValue: cellContent): void;
 }
 
 function cell(): cell {
   let contents: cellContent = cellContent.Empty;
 
-  function changeValue(newValue: cellContent.Crosses | cellContent.Noughts) {
+  function changeValue(newValue: cellContent) {
     contents = newValue;
   }
 
@@ -23,8 +23,10 @@ function cell(): cell {
   return { getValue, changeValue };
 }
 
+type gameBoard = [[cell, cell, cell], [cell, cell, cell], [cell, cell, cell]];
+
 function gameBoard() {
-  const board: cell[][] = [
+  const board: gameBoard = [
     [cell(), cell(), cell()],
     [cell(), cell(), cell()],
     [cell(), cell(), cell()],
@@ -52,6 +54,17 @@ function gameBoard() {
     console.log(
       board.map((boardRow) => boardRow.map((boardCell) => boardCell.getValue()))
     );
+  }
+
+  function isTie(): boolean {
+    for (const boardRow of board) {
+      for (const boardCell of boardRow) {
+        if (boardCell.getValue() === cellContent.Empty) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   function hasWon(player: player): boolean {
@@ -93,7 +106,7 @@ function gameBoard() {
     return false;
   }
 
-  return { markSquare, getBoard, showBoard, hasWon };
+  return { markSquare, getBoard, showBoard, isTie, hasWon };
 }
 
 interface player {
@@ -113,10 +126,26 @@ function player(name: string, mark: cellContent.Crosses | cellContent.Noughts) {
   return { getName, getMark };
 }
 
+interface gameOutcome {
+  endMessage(): string;
+}
+
+function win(player: player): gameOutcome {
+  return {
+    endMessage: () => `${player.getName()} wins!`,
+  };
+}
+
+function tie(): gameOutcome {
+  return {
+    endMessage: () => "Tie game!",
+  };
+}
+
 interface ticTacToeController {
-  playRound(row: number, column: number): player | undefined;
+  playRound(row: number, column: number): undefined | gameOutcome;
   getActivePlayer(): player;
-  getBoard(): cell[][];
+  getBoard(): gameBoard;
 }
 
 function ticTacToeController(
@@ -142,7 +171,7 @@ function ticTacToeController(
     );
   }
 
-  function playRound(row: number, column: number) {
+  function playRound(row: number, column: number): undefined | gameOutcome {
     const activePlayer = getActivePlayer();
     const moveResult = game.markSquare(activePlayer, row, column);
     if (typeof moveResult === "string") {
@@ -152,7 +181,12 @@ function ticTacToeController(
 
     if (game.hasWon(activePlayer)) {
       console.log(`${activePlayer.getName()} wins!`);
-      return activePlayer;
+      return win(activePlayer);
+    }
+
+    if (game.isTie()) {
+      console.log("Tie game!");
+      return tie();
     }
 
     activePlayerIndex = Number(!activePlayerIndex);
@@ -160,6 +194,119 @@ function ticTacToeController(
   }
 
   announceRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+    getBoard: game.getBoard,
+  };
+}
+
+function aiTicTacToeController(player1Name: string): ticTacToeController {
+  const game = gameBoard();
+  const realPlayer = player(player1Name, cellContent.Crosses);
+  const aiPlayer = player("AI", cellContent.Noughts);
+
+  function getActivePlayer() {
+    return realPlayer;
+  }
+
+  function playRound(row: number, column: number): undefined | gameOutcome {
+    const moveResult = game.markSquare(realPlayer, row, column);
+    if (typeof moveResult === "string") {
+      console.log(moveResult);
+      return;
+    }
+
+    if (game.hasWon(realPlayer)) {
+      console.log(`${realPlayer.getName()} wins!`);
+      return win(realPlayer);
+    }
+
+    if (game.isTie()) {
+      console.log("Tie game!");
+      return tie();
+    }
+
+    // AI turn
+    const [aiSelectedRow, aiSelectedColumn] = pickBestMove(aiPlayer);
+    game.markSquare(aiPlayer, aiSelectedRow, aiSelectedColumn);
+
+    if (game.hasWon(aiPlayer)) {
+      console.log(`${aiPlayer.getName()} wins!`);
+      return win(aiPlayer);
+    }
+
+    if (game.isTie()) {
+      console.log("Tie game!");
+      return tie();
+    }
+  }
+
+  function _lt(a: number, b: number) {
+    return a < b;
+  }
+
+  function _gt(a: number, b: number) {
+    return a > b;
+  }
+
+  function _switch_player(player: player) {
+    return player === aiPlayer ? realPlayer : aiPlayer;
+  }
+
+  function minimax(
+    board: gameBoard,
+    depth: number,
+    player: player,
+    isMaximizing: boolean
+  ): [number, number, number] {
+    if (game.hasWon(isMaximizing ? player : _switch_player(player))) {
+      return [10 - depth, -1, -1];
+    }
+    if (game.hasWon(isMaximizing ? _switch_player(player) : player)) {
+      return [-10 + depth, -1, -1];
+    }
+    if (game.isTie()) {
+      return [0, -1, -1];
+    }
+
+    let value: [number, number, number] = [isMaximizing ? -9999 : 9999, -1, -1];
+    const f = isMaximizing ? _gt : _lt;
+
+    for (let row = 0; row < 3; row++) {
+      for (let column = 0; column < 3; column++) {
+        const boardCell = board[row][column];
+        if (boardCell.getValue() !== cellContent.Empty) {
+          continue;
+        }
+        boardCell.changeValue(player.getMark());
+        const minimaxValue = minimax(
+          board,
+          depth + 1,
+          _switch_player(player),
+          !isMaximizing
+        )[0];
+
+        if (f(minimaxValue, value[0])) {
+          value = [minimaxValue, row, column];
+        }
+        boardCell.changeValue(cellContent.Empty);
+      }
+    }
+
+    return value;
+  }
+
+  function pickBestMove(player: player): [number, number] {
+    const [bestMoveRow, bestMoveColumn] = minimax(
+      game.getBoard(),
+      0,
+      player,
+      true
+    ).slice(1);
+    return [bestMoveRow, bestMoveColumn];
+  }
 
   return {
     playRound,
@@ -198,6 +345,30 @@ function PresentationController() {
     }
   }
 
+  function clickListener(clickEvent: MouseEvent) {
+    console.log(clickEvent);
+    const selectedCell = clickEvent.target as HTMLSpanElement;
+    if (!selectedCell.classList.contains("board-element")) {
+      console.log("not a grid cell, returning");
+      return;
+    }
+
+    const selectedRow = Number(selectedCell.getAttribute("data-row") as string);
+    const selectedColumn = Number(
+      selectedCell.getAttribute("data-column") as string
+    );
+
+    const possibleGameEnd = game.playRound(selectedRow, selectedColumn);
+    renderBoard();
+
+    if (possibleGameEnd) {
+      turnH1.textContent = possibleGameEnd.endMessage();
+
+      // clean up click listener to prevent further interaction
+      boardDiv.removeEventListener("click", clickListener);
+    }
+  }
+
   const playerNameForm = document.querySelector<HTMLFormElement>(".name-form");
   playerNameForm?.addEventListener("submit", (submitEvent) => {
     submitEvent.preventDefault();
@@ -206,28 +377,29 @@ function PresentationController() {
     const player1Name = formData.get("player1-name") as string;
     const player2Name = formData.get("player2-name") as string;
 
-    game = ticTacToeController(player1Name, player2Name);
+    if (!player2Name) {
+      game = aiTicTacToeController(player1Name);
+    } else {
+      game = ticTacToeController(player1Name, player2Name);
+    }
+
     renderBoard();
 
-    boardDiv.addEventListener("click", (clickEvent) => {
-      console.log(clickEvent);
-      const selectedCell = clickEvent.target as HTMLSpanElement;
-      if (!selectedCell.classList.contains("board-element")) {
-        console.log("not a grid cell, returning");
-        return;
-      }
-
-      const selectedRow = Number(
-        selectedCell.getAttribute("data-row") as string
-      );
-      const selectedColumn = Number(
-        selectedCell.getAttribute("data-column") as string
-      );
-
-      game.playRound(selectedRow, selectedColumn);
-      renderBoard();
-    });
+    boardDiv.addEventListener("click", clickListener);
   });
+
+  const submitButton = document.querySelector<HTMLButtonElement>(
+    "form > button"
+  ) as HTMLButtonElement;
+  playerNameForm?.addEventListener(
+    "submit",
+    (submitEvent) => {
+      submitEvent.preventDefault();
+
+      submitButton.textContent = "Reset";
+    },
+    { once: true }
+  );
 }
 
 PresentationController();
